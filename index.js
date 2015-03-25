a6051d4895dc61ff80df341ada22a78ecd313c99
Daniel Gaytan <dgaytan6@gmail.com>
2015-03-25 00:31:29 -0600

    Add views and assets

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'jade')
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
