48ee93db7fd4a494890071d3f2cfcb8a7722ff65
Daniel Gaytan <dgaytan6@gmail.com>
2015-03-25 13:34:38 -0600

    Add sass processor. Fix tag list spacing

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+
 var app = express();
 
 app.get('/', function (req, res) {
   res.render('index');
 });
+
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'assets'),
+  dest: path.join(__dirname, 'public'),
+  debug: true,
+  outputStyle: 'expanded'
+}));
 
 app.use(express.static('public'));
 app.set('views', './views');
